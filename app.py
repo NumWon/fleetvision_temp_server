8990241385f02938f9f4d08fa2c47da9fc1da9ae
Naumaan <naumaansheikh30@gmail.com>
2024-07-10 00:06:10 -0400

    small fix

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ app = Flask(__name__)
 
 @app.route('/obd', methods=['POST'])
 def receive_obd_data():
-    data = request.json
-    print("Data recieved: ", data)
-    response_data = {
-        "status": "success",
-        "data": data,
-        "timestamp": datetime.now().isoformat()
-    }
-    return jsonify(response_data)
+    if request.is_json:
+        data = request.json
+        print("Data recieved: ", data)
+        response_data = {
+			"status": "success",
+			"data": data,
+			"timestamp": datetime.now().isoformat()
+		}
+        return jsonify(response_data)
+    else:
+        return jsonify({"status": "error", "message": "Invalid request"}), 400
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
